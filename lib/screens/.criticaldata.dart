6cec5f066bb1276b14a042de41c24d2c90b54034
kristinakhristi95 <krishchristian.9@gmail.com>
2025-04-16 10:39:07 -0400

    updated

--- lib/screens/.criticaldata.dart
+++ lib/screens/.criticaldata.dart
@@ -1,14 +1,14 @@
-r// Blood Pressure
+// Blood Pressure
 //120/80 mmHg
 
 
 // Heartbeat Rate
-//60 – 100 beats per minute (bpm)
+//60 – 100 beats per minute (bpm), > 60,20
 
 
 // Blood Oxygen Level
-//95% – 100% spo2
+//95% – 100% spo2, > 95,60
 
 
 // Respiratory Rate
-//12 – 20 breaths per minute
+//12 – 20 breaths per minute > 12, 9
